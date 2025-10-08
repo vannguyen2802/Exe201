f0368babcf6867adecdde75989301ffba6289270
nguyengithub123 <ph.nguyen0704@gmail.com>
2025-10-08 07:19:18 +0700

    v1_momo

--- Nestera/app/src/main/java/com/example/nestera/Activity/ThanhToan_Activity.java
+++ Nestera/app/src/main/java/com/example/nestera/Activity/ThanhToan_Activity.java
@@ -18,6 +18,8 @@ import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import vn.momo.momo_partner.AppMoMoLib;
+import vn.momo.momo_partner.MoMoParameterNameMap;
 import com.example.nestera.Adapter.HoaDon_Adapter;
 import com.example.nestera.Adapter.NganHangSpinner_Adapter;
 import com.example.nestera.Dao.NganHangDao;
@@ -26,10 +28,15 @@ import com.example.nestera.R;
 import com.example.nestera.model.HoaDon;
 import com.example.nestera.model.NganHang;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ThanhToan_Activity extends AppCompatActivity {
     Spinner spNganHang;
@@ -50,12 +57,19 @@ public class ThanhToan_Activity extends AppCompatActivity {
     byte[] anhqr;
     int mahoadon;
     final int REQUEST_CODE_FOLDER = 456;
+    private String amount = "10000";
+    private String fee = "0";
+    int environment = 0;//developer default
+    private String merchantName = "NESTERA";
+    private String merchantCode = "MOMOYQVY20251008";
+    private String merchantNameLabel = "NESTERA";
+    private String description = "Thanh toán dịch vụ tro";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thanh_toan);
-
+        AppMoMoLib.getInstance().setEnvironment(AppMoMoLib.ENVIRONMENT.DEVELOPMENT); // AppMoMoLib.ENVIRONMENT.PRODUCTION
 
         spNganHang=findViewById(R.id.spnNganHang);
         imgAnhQR_tt=findViewById(R.id.imgAnhQR_tt);
@@ -135,20 +149,100 @@ public class ThanhToan_Activity extends AppCompatActivity {
         });
 
     }
+    //Get token through MoMo app
+    private void requestPayment() {
+        AppMoMoLib.getInstance().setAction(AppMoMoLib.ACTION.PAYMENT);
+        AppMoMoLib.getInstance().setActionType(AppMoMoLib.ACTION_TYPE.GET_TOKEN);
+//        if (mahoadon.getText().toString() != null && edAmount.getText().toString().trim().length() != 0)
+//            amount = edAmount.getText().toString().trim();
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if (requestCode == REQUEST_CODE_FOLDER && resultCode == RESULT_OK && data != null){
-            Uri uri = data.getData();
-            try {
-                InputStream inputStream = getContentResolver().openInputStream(uri);
-                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
-                imgAnhThanhToan.setImageBitmap(bitmap);
-            } catch (FileNotFoundException e) {
-                throw new RuntimeException(e);
-            }
+        Map<String, Object> eventValue = new HashMap<>();
+        //client Required
+        eventValue.put("merchantname", merchantName); //Tên đối tác. được đăng ký tại https://business.momo.vn. VD: Google, Apple, Tiki , CGV Cinemas
+        eventValue.put("merchantcode", merchantCode); //Mã đối tác, được cung cấp bởi MoMo tại https://business.momo.vn
+        eventValue.put("amount", amount); //Kiểu integer
+        eventValue.put("orderId", "orderId123456789"); //uniqueue id cho Bill order, giá trị duy nhất cho mỗi đơn hàng
+        eventValue.put("orderLabel", "Mã đơn hàng"); //gán nhãn
 
+        //client Optional - bill info
+        eventValue.put("merchantnamelabel", "Dịch vụ");//gán nhãn
+        eventValue.put("fee", 0); //Kiểu integer
+        eventValue.put("description", description); //mô tả đơn hàng - short description
+
+        //client extra data
+        eventValue.put("requestId",  merchantCode+"merchant_billId_"+System.currentTimeMillis());
+        eventValue.put("partnerCode", merchantCode);
+        //Example extra data
+        JSONObject objExtraData = new JSONObject();
+        try {
+            objExtraData.put("site_code", "008");
+            objExtraData.put("site_name", "CGV Cresent Mall");
+            objExtraData.put("screen_code", 0);
+            objExtraData.put("screen_name", "Special");
+            objExtraData.put("movie_name", "Kẻ Trộm Mặt Trăng 3");
+            objExtraData.put("movie_format", "2D");
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
-        super.onActivityResult(requestCode, resultCode, data);
+        eventValue.put("extraData", objExtraData.toString());
+
+        eventValue.put("extra", "");
+        AppMoMoLib.getInstance().requestMoMoCallBack(this, eventValue);
+
+
     }
+    //Get token callback from MoMo app an submit to server side
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == AppMoMoLib.getInstance().REQUEST_CODE_MOMO && resultCode == -1) {
+            if(data != null) {
+                if(data.getIntExtra("status", -1) == 0) {
+                    //TOKEN IS AVAILABLE
+//                    tvMessage.setText("message: " + "Get token " + data.getStringExtra("message"));
+                    String token = data.getStringExtra("data"); //Token response
+                    String phoneNumber = data.getStringExtra("phonenumber");
+                    String env = data.getStringExtra("env");
+                    if(env == null){
+                        env = "app";
+                    }
+
+                    if(token != null && !token.equals("")) {
+                        // TODO: send phoneNumber & token to your server side to process payment with MoMo server
+                        // IF Momo topup success, continue to process your order
+                    } else {
+//                        tvMessage.setText("message: " + this.getString(R.string.not_receive_info));
+                    }
+                } else if(data.getIntExtra("status", -1) == 1) {
+                    //TOKEN FAIL
+                    String message = data.getStringExtra("message") != null?data.getStringExtra("message"):"Thất bại";
+//                    tvMessage.setText("message: " + message);
+                } else if(data.getIntExtra("status", -1) == 2) {
+                    //TOKEN FAIL
+//                    tvMessage.setText("message: " + this.getString(R.string.not_receive_info));
+                } else {
+                    //TOKEN FAIL
+//                    tvMessage.setText("message: " + this.getString(R.string.not_receive_info));
+                }
+            } else {
+//                tvMessage.setText("message: " + this.getString(R.string.not_receive_info));
+            }
+        } else {
+//            tvMessage.setText("message: " + this.getString(R.string.not_receive_info_err));
+        }
+    }
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        if (requestCode == REQUEST_CODE_FOLDER && resultCode == RESULT_OK && data != null){
+//            Uri uri = data.getData();
+//            try {
+//                InputStream inputStream = getContentResolver().openInputStream(uri);
+//                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+//                imgAnhThanhToan.setImageBitmap(bitmap);
+//            } catch (FileNotFoundException e) {
+//                throw new RuntimeException(e);
+//            }
+//
+//        }
+//        super.onActivityResult(requestCode, resultCode, data);
+//    }
 }
